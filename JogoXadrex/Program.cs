e8d8b5eff88cc4b752fc4ad4223f19557aaae43c
rob.nogueira@terra.com.br <stayto.rob@hotmail.com>
2020-04-01 11:22:03 -0300

    Comecando a colocar pecas no tabuleiro

--- JogoXadrex/Program.cs
+++ JogoXadrex/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace JogoXadrex {
     class Program {
@@ -7,8 +8,11 @@ namespace JogoXadrex {
 
             Tabuleiro tab = new Tabuleiro(8, 8);
 
+            tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
+
             Tela.imprimirTabuleiro(tab);
-            
+
         }
     }
 }
--- JogoXadrex/tabuleiro/Peca.cs
+++ JogoXadrex/tabuleiro/Peca.cs
@@ -10,8 +10,8 @@ namespace tabuleiro {
         public int qtdMovimentos { get; protected set; }
         public Tabuleiro tab { get; set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab) {
-            this.posicao = posicao;
+        public Peca( Cor cor, Tabuleiro tab) {
+            this.posicao = null;
             this.cor = cor;
             this.tab = tab;
             this.qtdMovimentos = 0;
